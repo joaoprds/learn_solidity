e7587b0e3360ee153d48c3ec200231e4057038d4
joaoprds <joaopaulorochadesousa@gmail.com>
2023-02-01 17:15:16 -0300

    get info to contract with link documentation

---
+++ contracts/BlockInfo.sol
@@ -0,0 +1,17 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract BaseInfo {
+
+    function retornaInfo() public view returns (uint){
+        //return block.timestamp;
+        //return block.number;
+        return block
+    }
+
+
+}
+
+
+//DOCUMENTAÇÃO DA SOLIDITY -> https://docs.soliditylang.org/en/v0.8.18/
+// LINK DO DIA 01/02/2023
